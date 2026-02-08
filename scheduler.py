68ad071cc9d09aef7ac5199f95a2f3fd8c601210
MadhavVP <vpmadhav@gmail.com>
2026-02-08 00:44:43 -0500

    Scheduler outline

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,8 @@
-# MOCK SCHEDULER MODULE
-# Implementing by teammate - this is just a placeholder for integration
+# SCHEDULER MODULE
+import threading
+import time
+import os
+from pymongo import Mongo
 
 def orchestrate_pipeline():
     """
@@ -10,14 +13,33 @@ def orchestrate_pipeline():
     3. Save to DB
     4. Notify users
     """
-    print("[MOCK] Scheduler: triggering pipeline...")
-    # Simulate some work
-    import time
-    time.sleep(1)
-    print("[MOCK] Scheduler: Pipeline finished.")
+    while True:
+        print("Beginning new legislation check")
+
+        client = Mongo(os.environ["MONGO_URI"])
+        db = client["users"]
+        collection = db["users"]
+
+        cursor = collection.find({})
+
+        for user in cursor:
+            tags = user.get('tags', [])
+            newlaws = []
+            for tag in tags:
+                print(tag)
+                links = [] #query nathan's thing
+                newlaws.append(links)
+            for law in newlaws:
+                popup = '' #query gabe's thing
+                #send a popup to user
+                    
+
+        time.sleep(1)
+        print("Ending new legislation check")
 
 def start_scheduler():
     """
     Mock scheduler start.
     """
+    threading.Thread(target=orchestrate_pipeline)
     print("[MOCK] Scheduler: Started background jobs.")
